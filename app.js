3d31f68b1322f78f3c3c34a65fd75c3b6900c227
Hasanur20 <99091516+Hasanur20@users.noreply.github.com>
2024-09-13 18:56:37 +0100

    trying event listener method
    added a new event listener to button submit to try get successful submit alert onsubmit

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('simple-form');
-    const responseMessage = document.getElementById('responseMessage');
-  
-    form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevent default form submission
-  
-      const formData = new FormData(form);
-  
-      // Sending the form data using the Fetch API
-      fetch('https://script.google.com/macros/s/AKfycbwUuZ80iQyx24SGmX6VYzUZkWBCmmvqWETdQx83WXAMwiEpZX6gWGwG0IuUF74FtWXGZw/exec', {
-        method: 'POST',
-        body: formData,
-      })
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('simple-form');
+  const responseMessage = document.getElementById('responseMessage');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent default form submission
+
+    const formData = new FormData(form);
+
+    // Sending the form data using the Fetch API
+    fetch('https://script.google.com/macros/s/AKfycbwUuZ80iQyx24SGmX6VYzUZkWBCmmvqWETdQx83WXAMwiEpZX6gWGwG0IuUF74FtWXGZw/exec', {
+      method: 'POST',
+      body: formData,
+    })
       .then(response => response.json()) // Handle JSON response if necessary
       .then(data => {
         // Display success message
-        responseMessage.textContent = 'Form submitted successfully!';
-        function showAlert(){
-            alert(responseMessage.textContent)
-        }
+        document.getElementById("submit").addEventListener("click", function () { alert("Congrats"); });
+        // responseMessage.textContent = 'Form submitted successfully!';
         /*  console.log('Success:', data);*/
       })
       .catch(error => {
         // Handle error
-        responseMessage.textContent = 'An error occurred. Please try again.';
-        showAlert()
+        alert("Error, please try again")
+        // responseMessage.textContent = 'An error occurred. Please try again.';
         /*  console.error('Error:', error); */
       });
-    });
   });
-  
+});
